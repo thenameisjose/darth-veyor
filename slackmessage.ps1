b57bca03a90d362edbbc5d983fba02111d8ea7de
Jose Cortes <jcortes@defisolutions.com>
2019-03-21 14:49:59 -0500

    updated yaml

---
+++ slackmessage.ps1
@@ -0,0 +1,131 @@
+param([string]$token="xoxp-101647422818-102332869287-220469312550-38e9143c414aaf5731679544e5dd41df",
+[string]$mode="",
+[string]$channel="#deploy",
+[string]$messageid="",
+[string]$status="")
+ 
+$text = " "
+$postUrl = "https://slack.com/api/chat.postMessage"
+$updateUrl = "https://slack.com/api/chat.update"
+$iconUrl = "https://pbs.twimg.com/profile_images/1604347359/logo_512x512_normal.png"
+
+$selectedAttachment = switch ( $status )
+    {
+        success 
+        {
+            '[{
+                "fallback": "Required plain-text summary of the attachment.",
+                "color": "#228B22",
+                "fields": [
+                    {
+                        "title": "Success",
+                        "value": "Deploy LMS release to UAT",
+                        "short": false
+                    }
+                ]
+            }]'
+        }
+        failed 
+        {
+            '[{
+                "fallback": "Required plain-text summary of the attachment.",
+                "color": "#FF0000",
+                "fields": [
+                    {
+                        "title": "Failed",
+                        "value": "Deploy LMS release to UAT",
+                        "short": false
+                    }
+                ]
+            }]'
+
+        }
+        default 
+        { 
+            '[{
+                "fallback": "Required plain-text summary of the attachment.",
+                "color": "#FFFF00",
+                "fields": [
+                    {
+                        "title": "Pending",
+                        "value": "Deploy LMS release to UAT",
+                        "short": false
+                    }
+                ]
+            }]'
+        }
+    }
+
+
+if($mode -eq "post"){
+    $postSlackMessage = @{
+        text=$text;
+        token=$token;
+        channel=$channel;
+        attachments = $selectedAttachment;
+        username="LMS Deploy";
+        icon_url=$iconUrl
+    }
+
+    $response = Invoke-RestMethod -Uri $postUrl -Body $postSlackMessage 
+
+    if($response.ok)
+    {
+        $messageId = $response.ts
+        $channelId = $response.channel
+        Write-Host "Call success!:" $response
+        Set-AppveyorBuildVariable 'slack_message_id' $messageId
+        Set-AppveyorBuildVariable 'slack_message_channel' $channelId
+    }
+    else
+    {
+        Write-Host "Call failed!:" $response
+    }
+}
+
+    
+
+if($mode -eq "update") {
+
+    $messageId = Get-AppveyorBuildVariable 'slack_message_id' 
+    $channelId = Get-AppveyorBuildVariable 'slack_message_channel' 
+
+    if (-not ([string]::IsNullOrEmpty($messageId)))
+    {
+    $postSlackMessage = @{
+        text=$text;
+        token=$token;
+        channel=$channelId;
+        ts=$messageID;
+        attachments=$selectedAttachment;
+        username="LMS Deploy";
+        icon_url=$iconUrl}
+
+
+        $response = Invoke-RestMethod -Uri $updateUrl -Body $postSlackMessage 
+    }
+    else
+    {
+     $postSlackMessage = @{
+        text=$text;
+        token=$token;
+        channel=$channel;
+        attachments = $selectedAttachment;
+        username="LMS Deploy";
+        icon_url=$iconUrl}
+        
+        $response = Invoke-RestMethod -Uri $postUrl -Body $postSlackMessage 
+    }
+
+    if($response.ok)
+    {
+        #$messageId = $response.ts
+        #$channelId = $response.channel
+        Write-Host "Update Call success!:" $response
+        
+    }
+    else
+    {
+        Write-Host "Update Call failed!:" $response
+    }
+}
