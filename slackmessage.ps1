cc870e9a517e24b600cdd897cd380c28df69328d
Jose Cortes <thenameisjose@gmail.com>
2019-04-04 13:23:32 -0500

    Feat slack notice (#18)
    * SERV-xxxx - real commit message from PR 1
    
    * SERV-xxxx - real commit message from PR 1.1
    
    * SERV-xxxx - real commit message from PR 1.2
    
    * SERV-xxxx - real commit message from PR 1.2 with body

--- slackmessage.ps1
+++ slackmessage.ps1
@@ -6,6 +6,7 @@ param([string]$token="",
 [string]$branch="master"
 )
  
+
  
  Write-Host "APPVEYOR_PULL_REQUEST_NUMBER:" $env:APPVEYOR_PULL_REQUEST_NUMBER
  Write-Host "APPVEYOR_REPO_BRANCH:" $env:APPVEYOR_REPO_BRANCH
@@ -19,7 +20,7 @@ param([string]$token="",
 
  if ([string]::IsNullOrEmpty($env:git_message_text))
 {
-	$gitData = ConvertFrom-StringData (git log -1 --no-merges --format=format:"commitId=%H%nmessage=%s%ncommitted=%aD" | out-string)
+	$gitData = ConvertFrom-StringData (git log -1 --no-merges --format=format:"commitId=%H%nmessage=%B%ncommitted=%aD" | out-string)
 	if ($gitData['message'] -eq "") { $gitData['message'] = "No commit message available for $($gitData['commitid'])" }
 	$env:git_message_text = $gitData['message'] 
 }
